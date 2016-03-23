cc6cd5a11406204c35a0df78e65c0d60172b3962
wwuyts <wemawuyts@gmail.com>
2016-03-23 17:36:49 +0100

    init

---
+++ src/React-Template/client/app/main.tsx
@@ -0,0 +1,11 @@
+﻿/// <reference path="../typings/tsd.d.ts" />
+import * as React from 'react';
+
+export class HelloWorld extends React.Component<any, any> {
+   render() {
+      return <h1>Hello from {this.props.phrase}!</h1>;
+   }
+}
+
+
+ 
